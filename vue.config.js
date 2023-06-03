75bb1096a1e01aaedb2701c6b18511f860b8800a
Dan Kennedy <dan@mojoplease.com>
2023-06-03 13:16:32 +0400

    Fix for deploying and updated config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,10 @@
-const {defineConfig} = require('@vue/cli-service')
-
-module.exports = defineConfig({
-    configureWebpack: {
-        entry: "./src/main.js",
-        devServer: {
-            hot: true,
-        },
-        watch: true,
-        watchOptions: {
-            ignored: /node_modules/,
-            poll: 1000,
-        },
+module.exports = {
+  publicPath: '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'AnnuCal',
     },
-    transpileDependencies: true,
-});
+  }
+};
